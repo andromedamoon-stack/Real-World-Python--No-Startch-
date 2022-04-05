dd9dd8cfacc434fde7109bfc7d8b9f74e5041abd
Sarah W <83520578+andromedamoon-stack@users.noreply.github.com>
2022-04-05 04:21:03 +0000

    set up search class, variables, and added cape map

--- bayes.py
+++ bayes.py
@@ -13,5 +13,29 @@ SA2_CORNERS = (80, 255, 130, 305) #(UL-X, UL-Y, LR-X, LR-Y)
 SA3_CORNERS = (105,205,155, 255) #(UL-X, UL-Y, LR-X, LR-Y)
 
 #defines search class
-def search():
-  
+class search():
+  "Bayesian search & rescue game."
+  def __init__(self,name):
+    self.name = name
+    self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
+    if self.img is None:
+      print('Could not load map file {}'.format(MAP_FILE), file=sys.stderr)
+      sys.exit(1)]
+
+    self.area_actual = 0
+    self.sailor_actual = [0, 0] # local cord within search area
+
+    self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3], SA1_CORNERS[0] : SA1_CORNERS[2]]
+
+    self.sa2 = self.img[SA2_CORNERS[1] : SA2_CORNERS[3], SA2_CORNERS[0] : SA2_CORNERS[2]]
+
+    self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3], SA3_CORNERS[0] : SA3_CORNERS[2]]
+
+    self.p1 = 0.2
+    self.p2 = 0.5
+    self.p3 = 0.3
+
+    self.sept1 = 0
+    self.sept2 = 0
+    self.sept3 = 0
+
